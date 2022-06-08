73305da64cef51133bad7b07b98ec39fd377a563
QuanDA <quanda@gmail.com>
2022-06-08 15:26:02 +0700

    Initial Commit

---
+++ src/main/java/com/example/gitintell/GitIntellApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitintell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitIntellApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitIntellApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitintell/GitIntellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitintell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitIntellApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
